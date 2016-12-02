cf3b97472adb8375e3a73c59b81741769bc20d53
matthew wilkinson <matt@somerobots.com>
2016-12-02 16:35:35 +0000

    Initial Commit

---
+++ MWGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MWGallery
+//
+//  Created by Matthew Wilkinson on 02/12/2016.
+//  Copyright © 2016 Matthew Wilkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
